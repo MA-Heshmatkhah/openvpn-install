aec01f7e8a4ab2196d582cca006e2966d6af109f
M.A Heshmatkhah <maheshmatkhah.prg@gmail.com>
2018-04-02 22:49:28 +0430

    add duplicate-cn
    make clients able to use single cert from multiple devices.

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -250,6 +250,8 @@ dh dh.pem
 tls-auth ta.key 0
 topology subnet
 server 10.8.0.0 255.255.255.0
+duplicate-cn
+explicit-exit-notify 1
 ifconfig-pool-persist ipp.txt" > /etc/openvpn/server.conf
 	echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server.conf
 	# DNS
